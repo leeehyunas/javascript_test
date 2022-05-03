4a74d5879ae843bc5b30ae0a7756284861955b41
hyunas <nzzzzz0628@gmail.com>
2022-05-03 16:58:23 +0900

    study

---
+++ level1/220421_같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+  let answer = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i + 1]) answer.push(arr[i])
+    console.log(answer)
+  }
+  return answer
+}
+console.log(solution([1, 1, 3, 4]))
