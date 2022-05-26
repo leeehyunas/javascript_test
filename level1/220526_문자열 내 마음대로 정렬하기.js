d80d4a51692014741a07b67dd58d849c3a3d0b21
hyunas <nzzzzz0628@gmail.com>
2022-05-26 15:23:13 +0900

    good!

---
+++ level1/220526_문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,23 @@
+function solution(strings, n) {
+  // 배열 안에서 n(index)번째에 해당하는 알파벳 찾아서 정렬
+  // const answer = strings.sort((a, b) => 
+  //   a[n] === b[n] ?
+  //     a.localeCompare(b)
+  //   :
+  //     a[n].localeCompare(b[n])
+  // )
+  const answer = strings.sort((a, b) =>{
+    const first = a[n]
+    const second = b[n]
+    if (first === second) {
+      return (a > b) - (a < b)
+    } else {
+      console.log(first)
+      console.log(second)
+      return (first > second) - (first < second)
+    }
+  })
+  console.log(answer)
+  // 사전순서대로 정렬
+}
+solution(["sun", "bed", "car"], 1)
