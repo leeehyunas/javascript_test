1b95143aabf4608defc066c5fd1cdb64f68bda10
hyunas <nzzzzz0628@gmail.com>
2022-05-30 11:33:21 +0900

    study

--- level1/220526_문자열 내 마음대로 정렬하기.js
+++ level1/220526_문자열 내 마음대로 정렬하기.js
@@ -6,18 +6,29 @@ function solution(strings, n) {
   //   :
   //     a[n].localeCompare(b[n])
   // )
-  const answer = strings.sort((a, b) =>{
+  // const answer = strings.sort((a, b) =>{
+  //   const first = a[n]
+  //   const second = b[n]
+  //   if (first === second) {
+  //     return (a > b) - (a < b)
+  //   } else {
+  //     console.log(first)
+  //     console.log(second)
+  //     return (first > second) - (first < second)
+  //   }
+  // })
+  const answer = strings.sort((a,b) => {
     const first = a[n]
-    const second = b[n]
+    const second= b[n]
+    console.log((a > b) - (a < b))
+    console.log(b[0])
     if (first === second) {
       return (a > b) - (a < b)
     } else {
-      console.log(first)
-      console.log(second)
       return (first > second) - (first < second)
     }
   })
   console.log(answer)
   // 사전순서대로 정렬
 }
-solution(["sun", "bed", "car"], 1)
+solution(["sun", "bed", "aer"], 1)
